16603c0f1e71747cbf7297222fff25d32cea5025
chojongwan <79524474+chojongwan@users.noreply.github.com>
2024-05-20 11:07:43 +0900

    Update_PlayerDataManager임시구현
    PlayerDataManager와 GameManager 수정

--- Assets/Scripts/Chojongwa/PlayerDataManager.cs
+++ Assets/Scripts/Chojongwa/PlayerDataManager.cs
@@ -1,18 +1,32 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDataManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Dictionary<CharacterType, CharacterStats> characterStatsDictionary;
+
+    private void Awake()
     {
-        
+        characterStatsDictionary = new Dictionary<CharacterType, CharacterStats>
+        {
+            { CharacterType.Normal, new CharacterStats(CharacterType.Normal, 1, 1, 1) },
+            { CharacterType.Rare, new CharacterStats(CharacterType.Rare, 2, 2, 2) },
+            { CharacterType.Unique, new CharacterStats(CharacterType.Unique, 3, 3, 3) },
+            { CharacterType.Epic, new CharacterStats(CharacterType.Epic, 4, 4, 4) }
+        };
     }
 
-    // Update is called once per frame
-    void Update()
+    public CharacterStats GetCharacterStats(CharacterType characterType)
     {
-        
+        if (characterStatsDictionary.TryGetValue(characterType, out var stats))
+        {
+            // 스탯 복사하여 반환
+            return new CharacterStats(stats.characterType, stats.Life, stats.Damage, stats.Speed);
+        }
+        else
+        {
+            Debug.LogError("Character type not found");
+            return null;
+        }
     }
 }
